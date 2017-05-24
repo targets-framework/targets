afd9ba7daf0fbeba528c7594091c23dceec55251
Mac Heller-Ogden <mac@machellerogden.com>
2017-05-24 10:10:22 -0500

    updated to use answers

--- index.js
+++ index.js
@@ -1,39 +1,122 @@
 #!/usr/bin/env node
 'use strict';
 
-// packages
-const inquirer = require('inquirer');
 const _ = require('lodash');
-const argv = require('minimist')(process.argv.slice(2));
-
-// file modules
-const Prompts = require('./prompts');
-const getPrompts = require('./lib/getPrompts');
-const invokeTargets = require('./lib/invokeTargets');
+const chalk = require('chalk');
+const EventEmitter = require('events').EventEmitter;
+const LineWrapper = require('stream-line-wrapper');
+const Promise = require('bluebird');
+const Answers = require('answers');
 const printResults = require('./lib/printResults');
-const config = _.assign({}, require('./lib/config'), argv);
+const inquirer = require('inquirer');
 
-function applyOptions(options) {
-    let { targets } = options;
-    let resultsPromise;
-    if (config._.length) {
-        let payload = _.omit(config, '_');
-        payload.options = config._;
-        payload.targets = targets;
-        payload.prompts = getPrompts(payload);
-        resultsPromise = invokeTargets(payload);
-    } else {
-        resultsPromise = inquirer.prompt(Prompts(targets))
-            .then((payload) => {
-                payload.targets = targets;
-                return invokeTargets(payload);
-            });
-    }
-    return resultsPromise;
+function handleStream(stream, target) {
+    const prefix = `[${chalk.yellow(target.label || target.name)}] `;
+    console.log(`${prefix}listening`);
+    const lineWrapper = new LineWrapper({ prefix });
+    stream.stdout.pipe(lineWrapper).pipe(process.stdout);
+    stream.stderr.pipe(process.stderr);
+    stream.on('error', console.error);
 }
 
-function Targets(options){
-    return applyOptions(options)
+function getConfig(options = {}) {
+
+    const { name, targets } = options;
+    const answers = Answers({ name });
+
+    function getChoices(config) {
+        const { targets } = config;
+        const getterPairs = _.toPairs(targets);
+        return _.map(getterPairs, (pair) => {
+            const getter = pair[1];
+            const getterKey = pair[0];
+            const option = {
+                name: getter.label || getter.name,
+                value: getterKey
+            };
+            return option;
+        });
+    }
+
+    function getInitialPrompt(config) {
+        if (_.isEmpty(config._)) {
+            let prompts = [
+                {
+                    type: 'checkbox',
+                    name: 'targetNames',
+                    message: 'What information would you like to see?',
+                    choices: getChoices(config)
+                }
+            ];
+            return inquirer.prompt(prompts)
+                .then((answers) => {
+                    config._ = answers.targetNames;
+                    return config;
+                });
+        } else {
+            return config;
+        }
+    }
+
+    function getMissing(config) {
+        const { targets } = config;
+        const targetNames = config._;
+        const prompts = _.uniqBy(_.reduce(targetNames, (acc, targetName) => {
+            const namespace = targetName.split('.').shift();
+            const targetPrompts = _.map(targets[targetName].prompts, (prompt) => {
+                _.set(prompt, 'name', `${namespace}.${prompt.name}`);
+                return prompt;
+            });
+            acc = acc.concat(targetPrompts);
+            return acc;
+        }, []), 'name');
+        answers.configure('prompts', prompts);
+        return answers.get().then((c) => {
+            c._ = _.isEmpty(targetNames) ? c._ : targetNames;
+            return c;
+        });
+    }
+
+    function addTargets(config) {
+        config.targets = targets;
+        return config;
+    }
+
+    return answers.get()
+        .then(addTargets)
+        .then(getInitialPrompt)
+        .then(getMissing)
+        .then(addTargets);
+}
+
+function invokeTargets(config) {
+    const { targets } = config;
+    const targetNames = config._;
+    const pendingTargets = _.reduce(targetNames, (acc, targetName) => {
+        const target = targets[targetName];
+        if (_.isFunction(target)) {
+            let targetResponse = target(config);
+            if (targetResponse instanceof EventEmitter) {
+                handleStream(targetResponse, target);
+                acc[targetName] = Promise.resolve(null);
+            } else {
+                acc[targetName] = Promise.resolve(targetResponse).catch(() => 'unavailable');
+            }
+        } else {
+            console.log('no target found');
+        }
+        return acc;
+    }, {});
+    return Promise.props(pendingTargets)
+        .then((results) => {
+            config.results = results;
+            return config;
+        });
+}
+
+function Targets(options = {}) {
+    return getConfig(options)
+        .then(invokeTargets)
         .then(printResults)
         .catch(console.error);
 }
