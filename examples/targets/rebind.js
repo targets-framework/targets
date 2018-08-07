b8b44e5b76788414eea72f400ceff036b7acc704
Mac Heller-Ogden <mac@machellerogden.com>
2018-08-06 23:20:03 -0500

    throw more errors, bind-to, exit-when, proceed-when and symbols for flags

--- examples/targets/rebind.js
+++ examples/targets/rebind.js
@@ -1,4 +1,4 @@
 'use strict';
 // rebind example on a composite target
 // try invoking invoke with: ./examples/mycli --logger.location Seattle rebind
-module.exports = [ '@logger.location::weather.location', 'weather' ];
+module.exports = [ '@prompts/off', '@logger.location::weather.location', 'weather' ];
